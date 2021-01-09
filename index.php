a75dd23ffc37483d548b449bac5d564b69f1c6e8
Julija Stirbė <julija.balsytee@gmail.com>
2021-01-09 23:02:48 +0200

    add data representation to table

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "mysql";
+$dbname = "project_manager_php";
+
+$connection = mysqli_connect($servername, $username, $password, $dbname);
+
+if (!$connection) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -17,11 +30,43 @@
     ?>
 
     <main class="main">
-        <p>Dėstymas</p>
+        <table class="data">
+            <thead class="data__head">
+                <tr class="data__row">
+                    <th class="data__column data__column--head">Id</th>
+                    <th class="data__column data__column--head">Project</th>
+                    <th class="data__column data__column--head">Action</th>
+                </tr>
+            </thead>
+            <tbody class="data__body">
+                <?php
+                $sql = "SELECT project_id, project_title FROM projects";
+                $result = mysqli_query($connection, $sql);
+                $idNo = 1;
+                if (mysqli_num_rows($result) > 0) {
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        print("<tr class='data__body-row'>");
+                        print("<td class='data__column'>" . $idNo++ . "</td>");
+                        print("<td class='data__column'>" . $row["project_title"] . "</td>");
+                        print("<td class='data__column'></td>");
+                        print("</tr>");
+                    }
+                } else {
+                    print("<tr class='data_body-row'>
+                            <td class='data__column'></td>
+                            <td class='data__column'></td>
+                            <td class='data__column'></td>
+                        </tr>");
+                }
+                ?>
+            <tbody>
+        </table>
     </main>
 
     <?php
-    include "footer.php"
+    include "footer.php";
+
+    mysqli_close($connection);
     ?>
 </body>
 
